efb94feaffb456e55d0ccf8351119d0a234ff698
Olivier Meunier <olivier@neokraft.net>
2020-06-13 20:33:58 +0200

    Added pkg/assets package.
    Not useful yet, needs the web/dist folder.

--- tools/generate_assets.go
+++ tools/generate_assets.go
@@ -7,11 +7,21 @@ import (
 
 	"github.com/shurcooL/vfsgen"
 
+	"github.com/readeck/readeck/pkg/assets"
 	"github.com/readeck/readeck/pkg/extract/fftr"
 )
 
 func main() {
 	var err error
+	if err = vfsgen.Generate(assets.Assets, vfsgen.Options{
+		Filename:     "pkg/assets/assets_vfsdata.go",
+		PackageName:  "assets",
+		BuildTags:    "assets",
+		VariableName: "Assets",
+	}); err != nil {
+		log.Fatalln(err)
+	}
+
 	if err = vfsgen.Generate(fftr.SiteConfigFolder, vfsgen.Options{
 		Filename:     "pkg/extract/fftr/siteconfig_vfsdata.go",
 		PackageName:  "fftr",
