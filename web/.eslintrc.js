828f712cee301d1b32cc23bcf73f55114afbc8bb
Olivier Meunier <olivier@neokraft.net>
2021-03-10 20:05:44 +0100

    eslintrc and browserlistrc

---
+++ web/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+  },
+  extends: [
+  ],
+  parserOptions: {
+    // parser: "babel-eslint",
+    ecmaVersion: 2019,
+    sourceType: "module",
+  },
+  plugins: [
+  ],
+  overrides: [
+  ],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+
+    "quotes": ["error", "double"],
+    "semi": ["error", "never"],
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "always-multiline",
+      "exports": "always-multiline",
+      "functions": "always-multiline",
+    }],
+    "comma-spacing": ["error", {"before": false, "after": true}],
+    "indent": ["error", 2],
+  },
+}
