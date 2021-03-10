d841d4a7282498b8463e559c7c012188bcae983d
Olivier Meunier <olivier@neokraft.net>
2021-03-10 20:05:46 +0100

    pkg/form, a form management tool
    Forms are bound to a struct and can be validated using predefined
    or custom rules. Validation returns the form values and any error
    than could arise.

---
+++ pkg/form/form.go
@@ -0,0 +1,223 @@
+package form
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"mime"
+	"net/http"
+	"net/url"
+	"reflect"
+	"strings"
+
+	"github.com/gorilla/schema"
+	"github.com/leebenson/conform"
+)
+
+var decoder = schema.NewDecoder()
+
+type (
+	// Form contains the data instance and the derived fields that
+	// can be used in a template or returned in a JSON response.
+	Form struct {
+		instance interface{}
+		Fields   map[string]*Field
+		Errors   Errors
+	}
+
+	// Field represents a form field.
+	Field struct {
+		instance reflect.Value
+		Name     string
+		Type     reflect.Type
+		Errors   Errors
+	}
+
+	// Errors is a error list that is attached to a form or a field.
+	Errors []error
+
+	// Validator is the interface for types implementing a validation
+	// routine after the data had been converted.
+	Validator interface {
+		Validate(*Form)
+	}
+)
+
+// MarshalJSON performs the field's JSON serialization.
+func (f *Field) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Value  interface{} `json:"value"`
+		Errors Errors      `json:"errors"`
+	}{
+		f.Value(),
+		f.Errors,
+	})
+}
+
+// Value returns the field's value.
+func (f *Field) Value() interface{} {
+	return f.instance.Interface()
+}
+
+// MarshalJSON performs the error list's JSON serialization.
+func (e Errors) MarshalJSON() ([]byte, error) {
+	if len(e) == 0 {
+		return json.Marshal(nil)
+	}
+
+	res := make([]string, len(e))
+	for i := range e {
+		res[i] = e[i].Error()
+	}
+
+	return json.Marshal(res)
+}
+
+// Add adds an new error to the list.
+func (e *Errors) Add(err error) {
+	*e = append(*e, err)
+}
+
+// NewForm creates a new form instance and defines the derived fields.
+func NewForm(instance interface{}) *Form {
+	val := reflect.ValueOf(instance)
+	if val.Kind() != reflect.Ptr {
+		panic("Form instance must be a pointer to a struct")
+	}
+	val = reflect.Indirect(val)
+	if val.Kind() != reflect.Struct {
+		panic("Form instance must be a pointer to a struct")
+	}
+	ret := &Form{
+		instance: instance,
+		Fields:   map[string]*Field{},
+	}
+
+	for i := 0; i < val.NumField(); i++ {
+		fd := val.Field(i)
+		// Exclude private field
+		if !fd.CanInterface() {
+			continue
+		}
+
+		tp := val.Type().Field(i)
+		name := tp.Name
+		tag := strings.SplitN(tp.Tag.Get("json"), ",", 2)[0]
+
+		// Exclude omitted fields
+		if tag == "-" {
+			continue
+		}
+
+		// Set field name
+		if tag != "" {
+			name = tag
+		}
+
+		o := &Field{
+			instance: fd,
+			Name:     tp.Name,
+			Type:     tp.Type,
+		}
+		ret.Fields[name] = o
+	}
+
+	return ret
+}
+
+// MarshalJSON performs the form's JSON serialization.
+func (f *Form) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		IsValid bool              `json:"is_valid"`
+		Errors  Errors            `json:"errors"`
+		Fields  map[string]*Field `json:"fields"`
+	}{
+		f.IsValid(),
+		f.Errors,
+		f.Fields,
+	})
+}
+
+// IsValid returns true when the form and its fields have no error
+func (f *Form) IsValid() bool {
+	if len(f.Errors) > 0 {
+		return false
+	}
+
+	for k := range f.Fields {
+		if len(f.Fields[k].Errors) > 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+// BindValues binds the values from any URL encoded value list.
+func (f *Form) BindValues(values url.Values) {
+	decoder.IgnoreUnknownKeys(true)
+	decoder.SetAliasTag("json")
+
+	err := decoder.Decode(f.instance, values)
+	if err != nil {
+		if err, ok := err.(schema.MultiError); ok {
+			for k, err := range err {
+				if _, ok := f.Fields[k]; ok {
+					if _, ok := err.(schema.ConversionError); ok {
+						f.Fields[k].Errors.Add(errors.New("Invalid input"))
+					} else {
+						f.Fields[k].Errors.Add(errors.New("Unknown error"))
+					}
+				} else {
+					f.Errors.Add(err)
+				}
+			}
+		}
+	}
+
+	conform.Strings(f.instance)
+}
+
+// BindJSON decodes a JSON payload from an io.Reader into the form instance.
+func (f *Form) BindJSON(r io.Reader) {
+	err := json.NewDecoder(r).Decode(f.instance)
+	if err != nil {
+		f.Errors.Add(err)
+		return
+	}
+
+	conform.Strings(f.instance)
+}
+
+// Validate performs the data validation on the form
+// instance when it exists.
+func (f *Form) Validate() {
+	if validator, ok := f.instance.(Validator); ok {
+		validator.Validate(f)
+	}
+}
+
+// Bind loads and validates the data using the method tied
+// to the request's content-type header.
+func Bind(f *Form, r *http.Request, validators ...func(f *Form)) {
+	mediaType, _, err := mime.ParseMediaType(r.Header.Get("content-type"))
+	if err != nil {
+		f.Errors.Add(errors.New("Invalid content-type"))
+		return
+	}
+
+	switch mediaType {
+	case "application/json", "text/json":
+		defer r.Body.Close()
+		f.BindJSON(r.Body)
+	case "application/x-www-form-urlencoded":
+		r.ParseForm()
+		f.BindValues(r.PostForm)
+	default:
+		f.Errors.Add(errors.New("Unknown content-type"))
+	}
+	for _, fn := range validators {
+		fn(f)
+	}
+	f.Validate()
+}
---
+++ pkg/form/validators.go
@@ -0,0 +1,68 @@
+package form
+
+import (
+	"errors"
+	"reflect"
+	"time"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+)
+
+// IsEmpty checks if a value is empty or not.
+// A value is considered empty if
+// - integer, float: zero
+// - bool: false
+// - string, array: len() == 0
+// - slice, map: nil or len() == 0
+// - interface, pointer: nil or the referenced value is empty
+func IsEmpty(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.String, reflect.Array, reflect.Map, reflect.Slice:
+		return v.Len() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Invalid:
+		return true
+	case reflect.Interface, reflect.Ptr:
+		if v.IsNil() {
+			return true
+		}
+		return IsEmpty(v.Elem())
+	case reflect.Struct:
+		v, ok := v.Interface().(time.Time)
+		if ok && v.IsZero() {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Required adds an error to the field when its value is empty.
+func Required(f *Field) {
+	if IsEmpty(f.instance) {
+		f.Errors.Add(errors.New("This field is required"))
+	}
+}
+
+// RequiredOrNull adds an error to the fields when its value is empty
+// except if it's null.
+func RequiredOrNull(f *Field) {
+	if !f.instance.IsNil() {
+		Required(f)
+	}
+}
+
+// IsEmail adds an error to the field if it's not a valid email address
+func IsEmail(f *Field) {
+	if err := validation.Validate(f.Value(), is.Email); err != nil {
+		f.Errors.Add(errors.New("Invalid email address"))
+	}
+}
