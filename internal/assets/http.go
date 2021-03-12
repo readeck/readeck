365ea588e29f1466361357954dca9bbb5ba81fc9
Olivier Meunier <olivier@neokraft.net>
2021-03-12 22:54:00 +0100

    Setup some cache control on assets.
    Assets served by embed.FS have no modified time. We give them the
    value of configs.BuildTime().
    
    For assets with a hash (ie. icons.2acb030a.svg) we set a max age of
    one year as well.

--- internal/assets/http.go
+++ internal/assets/http.go
@@ -4,12 +4,19 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path"
+	"regexp"
 	"strings"
 
 	"github.com/go-chi/chi/v5"
 
 	"github.com/readeck/readeck/assets"
+	"github.com/readeck/readeck/configs"
 	"github.com/readeck/readeck/internal/server"
+)
+
+var (
+	reAssetHashed = regexp.MustCompile(`\.[a-z0-9]{8}\.[a-z]+$`)
 )
 
 // SetupRoutes setup the static asset routes on /assets
@@ -58,5 +65,13 @@ func (f *directFileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.ServeContent(w, r, st.Name(), st.ModTime(), fd)
+	// Go 1.16 did not implement any form of caching control for embed.FS.
+	// Since all the files in assets/www have a hash fragment, we're just going to
+	// use it for caching.
+	if reAssetHashed.MatchString(path.Base(st.Name())) {
+		w.Header().Set("Cache-Control", `public, max-age=31536000`)
+	}
+
+	// And in any case, we use the build time as Last-Modified
+	http.ServeContent(w, r, st.Name(), configs.BuildTime(), fd)
 }
