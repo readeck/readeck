9e4c8df22ab9ba1616ff2c39a494fbf99bb40613
Olivier Meunier <olivier@neokraft.net>
2021-03-18 20:17:23 +0100

    Directly serve compressed assets when possible in assets/http

--- internal/assets/http.go
+++ internal/assets/http.go
@@ -1,12 +1,15 @@
 package assets
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"os"
 	"path"
+	"path/filepath"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 
@@ -43,6 +46,12 @@ func serveAssets() http.HandlerFunc {
 	}
 }
 
+var canditateEncodings [][2]string = [][2]string{
+	{"br", ".br"},
+	{"gzip", ".gz"},
+	{"", ""},
+}
+
 // directFileServer implements http.FileServer without the magic.
 // no redirect */index.html to */ and no directory listing.
 type directFileServer struct {
@@ -50,32 +59,80 @@ type directFileServer struct {
 }
 
 func (f *directFileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fd, err := f.root.Open(r.URL.Path)
-	if err != nil && os.IsNotExist(err) {
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		return
-	} else if err != nil {
-		http.Error(w, http.StatusText(500), 500)
-		return
-	}
-	defer fd.Close()
+	accepts := getAcceptEncodings(r)
+	name := filepath.Base(r.URL.Path)
+	mtime := configs.BuildTime()
 
-	st, err := fd.Stat()
-	if st.IsDir() {
-		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
-		return
-	} else if err != nil {
-		http.Error(w, http.StatusText(500), 500)
-		return
+	// Super shortchut for If-Modified-Since
+	ius := r.Header.Get("If-Unmodified-Since")
+	t, err := http.ParseTime(ius)
+	if err == nil {
+		m := mtime.Truncate(time.Second)
+		if m.Before(t) || m.Equal(t) {
+			w.WriteHeader(http.StatusNotModified)
+			return
+		}
 	}
 
-	// Go 1.16 did not implement any form of caching control for embed.FS.
-	// Since all the files in assets/www have a hash fragment, we're just going to
-	// use it for caching.
-	if reAssetHashed.MatchString(path.Base(st.Name())) {
-		w.Header().Set("Cache-Control", `public, max-age=31536000`)
-	}
+	// Loop over all encoding candidates and return the first matching file
+	// with the corresponding Content-Encoding when applicable.
+	for _, x := range canditateEncodings {
+		encoding := x[0]
+		ext := x[1]
+		last := encoding == ""
 
-	// And in any case, we use the build time as Last-Modified
-	http.ServeContent(w, r, st.Name(), configs.BuildTime(), fd)
+		if _, ok := accepts[encoding]; !ok && !last {
+			continue
+		}
+
+		fd, err := f.root.Open(fmt.Sprintf("%s%s", r.URL.Path, ext))
+		if err != nil && os.IsNotExist(err) {
+			if !last {
+				continue
+			}
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		} else if err != nil {
+			if !last {
+				continue
+			}
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+
+		defer fd.Close()
+		st, err := fd.Stat()
+		if st.IsDir() {
+			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+			return
+		} else if err != nil {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+
+		// Go 1.16 did not implement any form of caching control for embed.FS.
+		// Since all the files in assets/www have a hash fragment, we're just going to
+		// use it for caching.
+		if reAssetHashed.MatchString(path.Base(name)) {
+			w.Header().Set("Cache-Control", `public, max-age=31536000`)
+		}
+
+		if encoding != "" {
+			w.Header().Set("Content-Encoding", encoding)
+		}
+
+		// And in any case, we use the build time as Last-Modified
+		http.ServeContent(w, r, name, mtime, fd)
+		return
+	}
+}
+
+func getAcceptEncodings(r *http.Request) map[string]bool {
+	res := map[string]bool{}
+	ae := r.Header.Get("Accept-Encoding")
+
+	for _, x := range strings.Split(ae, ",") {
+		res[strings.TrimSpace(strings.Split(x, ";")[0])] = true
+	}
+	return res
 }
