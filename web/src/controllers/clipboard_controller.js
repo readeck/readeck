c99e94b9cbd6056d83adb2ac90f389023c32bf28
Olivier Meunier <olivier@neokraft.net>
2021-03-18 23:36:43 +0100

    Better copy controller. Fixed style for svg icons

--- web/src/controllers/clipboard_controller.js
+++ web/src/controllers/clipboard_controller.js
@@ -1,13 +1,22 @@
 import { Controller } from "stimulus"
+import icon from "../lib/icon"
 
 export default class extends Controller {
-  static get values () {
-    return {
-      jwt: String,
-    }
+  static get targets () {
+    return ["label", "content"]
+  }
+
+  connect () {
+    let el = document.createElement("button")
+    el.setAttribute("type", "button")
+    el.setAttribute("class", "button-clear")
+    el.setAttribute("data-action", `${this.identifier}#copy`)
+    el.appendChild(icon.getIcon("bxs-copy"))
+
+    this.labelTarget.appendChild(el)
   }
 
   async copy() {
-    await navigator.clipboard.writeText(this.jwtValue)
+    await navigator.clipboard.writeText(this.contentTarget.value)
   }
 }
--- web/src/lib/icon.js
+++ web/src/lib/icon.js
@@ -1,5 +1,22 @@
 const iconsURL = document.querySelector('html>head>meta[name="x-icons"]').content
 
+
+// getIcon returns an element with an svg icon for the given name.
+function getIcon(name) {
+  let e = document.createElement("span")
+  e.setAttribute("class", "svgicon")
+  let s = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+  s.setAttributeNS("http://www.w3.org/2000/svg", "viewbox", "0 0 100 100")
+  s.setAttributeNS("http://www.w3.org/2000/svg", "width", "16")
+  let u = document.createElementNS("http://www.w3.org/2000/svg", "use")
+
+  e.appendChild(s)
+  s.appendChild(u)
+
+  u.setAttributeNS(null, "href", `${iconsURL}#${name}`)
+  return e
+}
+
 // swapIcon changes the href of the first <use> tag in the
 // given svg element.
 function swapIcon(el, name) {
@@ -12,5 +29,6 @@ function swapIcon(el, name) {
 }
 
 export default {
+  getIcon,
   swapIcon,
 }
