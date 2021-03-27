9374aa53df87d1d63c0ac410892d234e55de20e3
Olivier Meunier <olivier@neokraft.net>
2021-03-27 01:08:16 +0100

    In turbo-form controller, conditionally change attributes.
    If the controller does not give any action or method, don't set them
    at all. It will then reuse the existing values.

--- web/src/controllers/turbo_form_controller.js
+++ web/src/controllers/turbo_form_controller.js
@@ -10,18 +10,23 @@ export default class extends Controller {
 
   connect() {
     let tagName = this.element.tagName.toLowerCase()
-
     switch (tagName) {
     case "form":
-      this.element.setAttribute("action", this.actionValue)
-      this.element.setAttribute("method", this.methodValue)
+      this.conditionnalAttr("action", this.actionValue, this.hasActionValue)
+      this.conditionnalAttr("method", this.methodValue, this.hasMethodValue)
       break
     case "button":
-      this.element.setAttribute("formaction", this.actionValue)
-      this.element.setAttribute("formmethod", this.methodValue)
+      this.conditionnalAttr("formaction", this.actionValue, this.hasActionValue)
+      this.conditionnalAttr("formmethod", this.methodValue, this.hasMethodValue)
       break
     default:
       throw new Error("turbo-form can only be used on form and button elements")
+    }
+  }
+
+  conditionnalAttr(name, value, condition) {
+    if (condition) {
+      this.element.setAttribute(name, value)
     }
   }
 }
