6366b61cbe0a39230f6625ddae397264f2c16b08
Olivier Meunier <olivier@neokraft.net>
2021-03-27 01:12:22 +0100

    observe mutations on elements in a turbo-refresh.
    Instead of checking the presence of a target, we'll check the
    presence of a given selector in the controller's element children.
    
    We do so when the controller connects and then on any mutation in the
    controller. As long as the selector is present, a refresh is perform
    with a timeout.

--- web/src/controllers/turbo_refresh_controller.js
+++ web/src/controllers/turbo_refresh_controller.js
@@ -5,42 +5,63 @@ import { Controller } from "stimulus"
 // This replaces a meta refresh of the full page and can be used on
 // several frames on the same page.
 export default class extends Controller {
-  static get targets () {
-    return ["loaded"]
-  }
   static get values () {
     return {
+      // A CSS selector that triggers the refresh when present
+      on: String,
+      // The page source to load, uses window.location if none
       src: String,
+      // Refresh every given seconds
       interval: Number,
     }
   }
 
-  connect() {
-    if (this.isLoaded()) {
-      return
+  connect () {
+    if (!this.onValue) {
+      throw new Error(`you must set data-${this.identifier}-on-value on the component`)
     }
 
     if (!this.hasSrcValue) {
       this.srcValue = window.location.href
     }
 
-    if (!this.hasIntervalValue) {
-      this.intervalValue = 5
+    // We need this to check the selector on every mutation
+    // console.log("connect", new Date())
+    this.timeout = null
+
+    if (!this.isLoaded()) {
+      this.check()
     }
 
-    let interval = window.setInterval(async () => {
+    this.observer = new MutationObserver(() => this.check())
+    this.observer.observe(this.element, { attributes: true, childList: true, subtree: true })
+  }
+
+  disconnect () {
+    this.observer.disconnect()
+  }
+
+  async check () {
+    await this.element.loaded
+    if (this.isLoaded()) {
+      return
+    }
+    if (this.timeout !== null) {
+      return
+    }
+    // console.log("must check", new Date())
+
+    this.timeout = window.setTimeout(async () => {
+      // console.log("check", new Date(), this.srcValue)
       this.element.src = this.srcValue
       await this.element.loaded
+
       this.element.src = null
-
-      if (this.isLoaded()) {
-        window.clearInterval(interval)
-      }
-
+      this.timeout = null
     }, this.intervalValue*1000)
   }
 
   isLoaded () {
-    return this.loadedTargets.length > 0
+    return this.element.querySelector(this.onValue) === null
   }
 }
