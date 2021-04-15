b0901339836a10f148aab0f12519968ce78199be
Olivier Meunier <olivier@neokraft.net>
2021-04-15 23:39:11 +0200

    Set Permissions-Policy header.

--- internal/server/security.go
+++ internal/server/security.go
@@ -115,8 +115,9 @@ func (s *Server) SetSecurityHeaders() func(next http.Handler) http.Handler {
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Referrer-Policy", "same-origin")
 			w.Header().Add("Content-Security-Policy", cspHeader)
+			w.Header().Set("Permissions-Policy", "interest-cohort=()")
+			w.Header().Set("Referrer-Policy", "same-origin")
 			w.Header().Add("X-Frame-Options", "DENY")
 			w.Header().Add("X-Content-Type-Options", "nosniff")
 			w.Header().Add("X-XSS-Protection", "1; mode=block")
